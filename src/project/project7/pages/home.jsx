9e7bc9c5c960f9f6e06edfb1cdbea41c85e85f84
nitesh <nitesh.khatri88@gmail.com>
2023-06-08 12:17:35 +0530

    added feature

--- src/project/project7/pages/home.jsx
+++ src/project/project7/pages/home.jsx
@@ -3,8 +3,22 @@ import Banner from '../components/banner';
 
 export default function Home() {
 
+  const [obj,setObj] = useState({
+    fname:'nitesh',
+    lname:'khatri',
+    age:34,
+    jobtitle:'fe developer',
+    address:{
+      line1:'test1',
+      line2:'test2'
+    },
+    friends:{
+      f1:'vijay',
+      f2:'vishal',
+      f3:'ronak'
+    }
+  });
   const [name,setName] = useState('');
-
   const [waitingList,setWaitinglist] = useState([]);
 
   const handleChange= (e) => {
@@ -22,6 +36,11 @@ export default function Home() {
     setWaitinglist([...waitingList])
   }
 
+  function handleChangeTwo(e){
+    e.preventDefault();
+    setObj({...obj,fname:'sameet',lname:'shah',address:{...obj.address,line1:'test3',line2:'test4'}})
+  }
+
 
   return (
     <div className='content'>
@@ -37,6 +56,16 @@ export default function Home() {
             )
           })}
         </ol>
+
+        <h1>{obj.fname} {obj.lname}</h1>
+        <hr/>
+        <h1>{obj.address.line1}</h1>
+        <h3>{...obj.address.line2}</h3>
+        <hr/>
+        <h4>{...obj.friends.f1}</h4>
+        <h4>{...obj.friends.f2}</h4>
+        <h4>{...obj.friends.f3}</h4>
+        <button onClick={handleChangeTwo}>Change</button>
     </div>
   )
 }
